765fb582a8065b5cceecc2e0d421d399878f8cf1
Viranch Mehta <email@viranch.me>
2018-03-15 22:27:53 +0530

    clean

--- src/server_doesnt_work.js
+++ src/server_doesnt_work.js
@@ -2,6 +2,7 @@ const path = require('path');
 const KoaStatic = require('koa-static');
 const Server = require('boardgame.io/server').Server;
 const TicTacToe = require('./game');
+
 const server = Server({ games: [TicTacToe] });
 server.app.use(KoaStatic(path.join(__dirname, '../build')));
 server.run(8000);
--- src/server_that_works.js
+++ src/server_that_works.js
@@ -3,12 +3,6 @@ import KoaStatic from 'koa-static';
 import { Server } from 'boardgame.io/server';
 import TicTacToe from './game';
 
-/*
-const path = require('path');
-const KoaStatic = require('koa-static');
-const Server = require('boardgame.io/server').Server;
-const TicTacToe = require('./game');
-*/
 const server = Server({ games: [TicTacToe] });
 server.app.use(KoaStatic(path.join(__dirname, '../build')));
 server.run(8000);
